909c0eb0ae34850b2dde3d35d73533326fbf802f
Alex <alexander.dawkins@gmail.com>
2021-04-14 21:35:35 +0100

    chore: fixed laggy menu

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -1,18 +1,18 @@
-window.onload = function () {
-    var sidebar = document.getElementById("sidebar");
-    var sidebarButton = document.getElementById("sidebar-button");
-    var closeSidebarButton = document.getElementById("close-sidebar");
+var sidebar = document.getElementById("sidebar");
+var sidebarButton = document.getElementById("sidebar-button");
+var closeSidebarButton = document.getElementById("close-sidebar");
 
-    function openSidebar(e) {
-        if (sidebar.className.indexOf("slideIn") !== -1) {
-            sidebar.className = sidebar.className.replace(" slideIn", "");
-        } else {
-            sidebar.className = sidebar.className + " slideIn";
-        }
+function openSidebar(e) {
+    e.stopPropagation(); e.preventDefault();
+    if (sidebar.className.indexOf("slideIn") !== -1) {
+        sidebar.className = sidebar.className.replace(" slideIn", "");
+    } else {
+        sidebar.className = sidebar.className + " slideIn";
     }
+}
 
 
-    sidebarButton.addEventListener("click", openSidebar, false);
-    closeSidebarButton.addEventListener("click", openSidebar, false);
-    sidebar.addEventListener('swiped-left', openSidebar, false);
-};
+sidebarButton.addEventListener("click", openSidebar, true);
+closeSidebarButton.addEventListener("click", openSidebar, true);
+sidebar.addEventListener('swiped-left', openSidebar, true);
+
