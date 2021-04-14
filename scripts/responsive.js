4c802b64d9502cde9bae7a2e73a11e478f90adcf
Alex <alexander.dawkins@gmail.com>
2021-04-14 21:15:28 +0100

    chore: mobile friendly closes #1

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -1,14 +1,18 @@
-window.onload = function() {
-  var sidebar = document.getElementById("sidebar");
-  var sidebarButton = document.getElementById("sidebar-button");
+window.onload = function () {
+    var sidebar = document.getElementById("sidebar");
+    var sidebarButton = document.getElementById("sidebar-button");
+    var closeSidebarButton = document.getElementById("close-sidebar");
 
-  function openSidebar(e) {
-    if (sidebar.className.indexOf("slideIn") != -1) {
-      sidebar.className = sidebar.className.replace(" slideIn", "");
-    } else {
-     sidebar.className = sidebar.className + " slideIn"; 
+    function openSidebar(e) {
+        if (sidebar.className.indexOf("slideIn") !== -1) {
+            sidebar.className = sidebar.className.replace(" slideIn", "");
+        } else {
+            sidebar.className = sidebar.className + " slideIn";
+        }
     }
-  }
 
-  sidebarButton.addEventListener("click", openSidebar, false);
+
+    sidebarButton.addEventListener("click", openSidebar, false);
+    closeSidebarButton.addEventListener("click", openSidebar, false);
+    sidebar.addEventListener('swiped-left', openSidebar, false);
 };
